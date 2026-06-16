9a45b468578c13444650f7edb6b6bde71d7cabb2
Ted Williams <williamsted1001@gmail.com>
2026-06-16 19:16:33 +0300

    Vercel view engine

--- server.js
+++ server.js
@@ -56,7 +56,8 @@ app.use(express.static('public'));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.set('view engine', 'ejs');
-app.set('views', './views');
+// app.set('views', './views');
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 
 app.use(express.urlencoded({

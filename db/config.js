7ae4de2175696fad3dc19ded6f60cbdf802d1b5d
Ted Williams <williamsted1001@gmail.com>
2026-06-16 17:36:21 +0300

    admin page 403 corrected

--- db/config.js
+++ db/config.js
@@ -3,17 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: String(process.env.DB_PASSWORD),
-    port: process.env.DB_PORT,
-})
 // const pool = new Pool({
-//     connectionString: process.env.NEON_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: String(process.env.DB_PASSWORD),
+//     port: process.env.DB_PORT,
 // })
+const pool = new Pool({
+    connectionString: process.env.NEON_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 export default pool;
